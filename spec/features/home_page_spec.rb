ad54a485c16522b6cc23483685198400ee097626
Logan Roberts <lroberts77@gmail.com>
2019-11-20 02:46:37 +0000

    Set up test for updating cart based on order

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature "Visitor navigates to home page", type: :feature, js: false do
+RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
 
   #SETUP
   before :each do
